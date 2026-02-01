33913b12e780fb864fe78739f2fcdd7fa95396c9
Rich Hildred <rhildred@gmail.com>
2026-02-01 01:11:51 +0000

    change to preact

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [preact()],
   server: {
     allowedHosts: true
   },
   resolve: {
     alias: {
       'react-native': 'react-native-web',
+      react: 'preact/compat',
+      'react-dom': 'preact/compat',
+      'react-dom/test-utils': 'preact/compat/test-utils',
+      'react/jsx-runtime': 'preact/compat/jsx-runtime',
+ 
     },
   },
 
